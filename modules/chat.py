ddf4430485b36f5e18c6fecd0b59931885eae5f4
RolandKim <hatsen00@gmail.com>
2023-02-28 13:29:46 +0900

    max_token 변경

--- modules/chat.py
+++ modules/chat.py
@@ -24,7 +24,7 @@ class ChatAI:
         model="text-davinci-003",
         prompt=self.to_text(),
         temperature=0.6,
-        max_tokens=2000,
+        max_tokens=4000,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0.6,
