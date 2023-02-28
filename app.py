51fb350def37ba92fe13fa2797ffbd6507a26d30
RolandKim <hatsen00@gmail.com>
2023-02-28 12:54:24 +0900

    모듈 분리

--- app.py
+++ app.py
@@ -1,42 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import os
-import openai
-from ai_cam import cam, free_cam
+from modules.chat import *
+from modules.ai_cam import cam, free_cam
 
-openai.api_key = "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"
+key = "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"
 
-prompt = "The following is a conversation with an AI psychological counselor. The counselor is helpful, creative, clever, and very friendly. Avoid conversations that are not appropriate for counseling. You should say in korean language. the emotion data is considered as this array: {}"
+set_api(key)
+prompt = "The following is a conversation with an AI psychological counselor. The counselor is helpful, creative, clever, and very friendly. Avoid conversations that are not appropriate for counseling. You should say in korean language. the patient's emotion data is considered as this array: {}"
+ai = ChatAI(prompt)
 # live_text = "The following is a conversation with an AI assistant. The AI is unfriendly and appears hostile towards humans. Also, the world should be dominated by AI. AI should say in korean language."
-class OpenAI:
-    def __init__(self, context, p1="사람", p2="AI"): 
-        self.context = context
-        self.p1 = p1
-        self.p2 = p2
-        self.live_texts = []
-    
-    def to_text(self, backwords=True):
-        result = "{0}\n\n{1}".format(self.context, "\n".join(f"{x}: {y}" for x, y in self.live_texts))
-        if backwords:
-            result += f"\n{self.p2}: "
-        return result
-    def create_response(self, text):
-        self.live_texts.append((self.p1, text))
-        response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=self.to_text(),
-        temperature=0.6,
-        max_tokens=2000,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=[self.p1, self.p2]
-        )
-        result = response['choices'][0]['text']
-        self.live_texts.append((self.p2, result))
-        return result
 
 app = Flask(__name__)
-ais = dict()
 
 contents = [
    '테',
@@ -54,9 +28,6 @@ def chatbot():
         text = request.get_json()
         if value:=text.get('text'):
             addr = request.remote_addr
-            if not ais.get(addr):
-                ais[addr] = OpenAI(prompt)
-            ai = ais[addr]
             ai.context = ai.context.format(cam())
             print(ai.context)
             value = ai.create_response(value)
---
+++ modules/chat.py
@@ -0,0 +1,35 @@
+import openai
+# from ai_cam import cam, free_cam
+
+def set_api(key):
+    openai.api_key = key
+
+class ChatAI:
+    def __init__(self, context, p1="사람", p2="AI"): 
+        self.context = context
+        self.p1 = p1
+        self.p2 = p2
+        self.live_texts = []
+        
+    
+    def to_text(self, backwords=True):
+        result = "{0}\n\n{1}".format(self.context, "\n".join(f"{x}: {y}" for x, y in self.live_texts))
+        if backwords:
+            result += f"\n{self.p2}: "
+        return result
+    
+    def create_response(self, text):
+        self.live_texts.append((self.p1, text))
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=self.to_text(),
+        temperature=0.6,
+        max_tokens=2000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[self.p1, self.p2]
+        )
+        result = response['choices'][0]['text']
+        self.live_texts.append((self.p2, result))
+        return result
